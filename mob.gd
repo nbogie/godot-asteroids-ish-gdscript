d31399d3f1d9799c8a5a6dd2afafcdd76de7a3b4
nbogie <neillbogie@googlemail.com>
2024-09-02 11:40:01 +0100

    velocity and position unused - mob is a rigidbody2d which handles those directly

--- mob.gd
+++ mob.gd
@@ -1,20 +1,16 @@
 extends RigidBody2D
 
-signal fire;
+signal fire(position, angle);
 
-@export var velocity:Vector2 = Vector2.ZERO
 @export var target = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
-	velocity = Vector2.from_angle(randf_range(0, 2 * PI)) * 4
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position += velocity
-	
+func _process(_delta: float) -> void:
 	var targetPos = Vector2(target.position)
 	var toTarget = targetPos - position
 	rotation = toTarget.angle() - PI/2
